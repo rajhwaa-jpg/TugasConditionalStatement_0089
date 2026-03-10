fc15a825e7c8c557d23624b6b36badd4391d7e2f
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-03-11 03:46:53 +0700

    Refactor code structure for improved readability and maintainability

--- pemdas.cpp
+++ pemdas.cpp
@@ -10,3 +10,7 @@ void inputData(float &berat, float &tinggi) {
     cout << "Masukkan tinggi badan (meter): ";
     cin >> tinggi;
 }
+// Fungsi untuk menghitung BMI
+float hitungBMI(float berat, float tinggi) {
+    return berat / (tinggi * tinggi);
+}

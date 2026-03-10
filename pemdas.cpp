d4aeffc4db4203d15c6a02ea66279ba90dd2eade
TugasConditionalStatement_0089 <rajhwaa@gmail.com>
2026-03-11 03:44:54 +0700

    Add inputData function for user data entry

--- pemdas.cpp
+++ pemdas.cpp
@@ -1,3 +1,12 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
+// Prosedur untuk memasukkan data
+void inputData(float &berat, float &tinggi) {
+    cout << "Masukkan berat badan (kg): ";
+    cin >> berat;
+    cout << "Masukkan tinggi badan (meter): ";
+    cin >> tinggi;
+}
